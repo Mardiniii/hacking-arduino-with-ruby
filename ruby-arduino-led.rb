7c29deaacc0b762b772bb2eb9afb64434553007b
mardiniii <sezama03@hotmail.com>
2015-10-20 16:20:48 -0500

    Change led file to show interface for user (Menu)

--- ruby-arduino-led.rb
+++ ruby-arduino-led.rb
@@ -1,10 +1,21 @@
 require 'artoo'
 require 'artoo-arduino'
+
 connection :arduino, :adaptor => :firmata, port: '/dev/cu.usbmodem1411'
 device :led, :driver => :led, :pin => 13
 
 work do
-  every 1.second do
-    led.on? ? led.off : led.on
+  while led_status != 3 do
+    puts "****************** MENU ******************"
+    puts "1. Encender el LED"
+    puts "2. Apagar el LED"
+    led_status = gets.chomp
+    if led_status == "2"
+      led.off
+      puts "LED Apagado"
+    elsif led_status == "1"
+      led.on
+      puts "LED Encendido"
+    end
   end
 end
