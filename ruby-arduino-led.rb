b6a731a901b26414eceed3206c8f5177bdce1737
mardiniii <sezama03@hotmail.com>
2015-11-28 08:49:56 -0500

    Refactor code to control two leds

--- ruby-arduino-led.rb
+++ ruby-arduino-led.rb
@@ -2,7 +2,8 @@ require 'artoo'
 require 'artoo-arduino'
 
 connection :arduino, :adaptor => :firmata, port: '/dev/cu.usbmodem1411'
-device :led, :driver => :led, :pin => 13
+device :led_2, :driver => :led, :pin => 12
+device :led_1, :driver => :led, :pin => 13
 
 work do
   while led_status != 3 do
@@ -11,10 +12,12 @@ work do
     puts "2. Apagar el LED"
     led_status = gets.chomp
     if led_status == "2"
-      led.off
+      led_1.off
+      led_2.off
       puts "LED Apagado"
     elsif led_status == "1"
-      led.on
+      led_1.on
+      led_2.on
       puts "LED Encendido"
     end
   end
